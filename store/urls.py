6823e4ed8bf975ba5bfefa0724fe8fad0b17b2c1
jaipuriapratik <jaipuria.p@husky.neu.edu>
2017-05-29 14:25:44 -0400

    Storefront

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.store,name="index"),
+
+    url(r'^$',views.store, name='index'),
+    url(r'^book/(\d+)', views.book_details, name='book_details'),
 ]
