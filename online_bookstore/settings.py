c3f483ab803d5aa1e398a25b8c68507dc84ecf67
jaipuriapratik <jaipuria.p@husky.neu.edu>
2017-07-20 21:33:39 -0400

    add image

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -169,7 +169,7 @@ STATICFILES_FINDERS = (
 		'compressor.finders.CompressorFinder',
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
 # STATICFILES_DIRS = (
